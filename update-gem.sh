b1e88078ae1e78e04c7b0182016a4a72b512ea48
Herman verschooten <Herman@verschooten.net>
2014-04-10 10:23:48 +0200

    Move assets to app folder

--- update-gem.sh
+++ update-gem.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 bower install
-rm -rf vendor/assets/javascripts/vendor
-rm -rf vendor/assets/stylesheets
-mkdir -p vendor/assets/javascripts/vendor
-mkdir -p vendor/assets/stylesheets
-cp bower_components/modernizr/modernizr.js vendor/assets/javascripts/vendor/.
-cp -R bower_components/foundation/js/foundation/ vendor/assets/javascripts/foundation/
-cp -R bower_components/foundation/scss/ vendor/assets/stylesheets/
-cp bower_components/foundation/scss/foundation/_settings.scss vendor/assets/.
+rm -rf app/assets/javascripts/vendor
+rm -rf app/assets/stylesheets
+mkdir -p app/assets/javascripts/vendor
+mkdir -p app/assets/stylesheets
+cp bower_components/modernizr/modernizr.js app/assets/javascripts/vendor/.
+cp -R bower_components/foundation/js/foundation/ app/assets/javascripts/foundation/
+cp -R bower_components/foundation/scss/ app/assets/stylesheets/
+cp bower_components/foundation/scss/foundation/_settings.scss app/assets/.
 
 # echo "Now update version.rb"
